825521f917a2cfaa0b79ee903ef65ad66607545c
Paul2G <paul.garcia.galeana@uabc.edu.mx>
2024-04-28 16:18:48 -0700

    feat: time line component

---
+++ src/data/navigation.ts
@@ -0,0 +1,6 @@
+export const headerMenu = [
+  { label: "Sobre mi", to: "#about-me" },
+  { label: "Experiencia", to: "#experience" },
+  { label: "Projectos", to: "#projects" },
+  { label: "Habilidades", to: "#skill" },
+];
