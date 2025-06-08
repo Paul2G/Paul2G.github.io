f7b92d7f5cf0a9ef56b74985f165f4831c9d15cf
Paul2G <pggaleana@gmail.com>
2025-06-08 02:37:55 -0700

    refactor: change links in footer and header

---
+++ src/data/misc.ts
@@ -0,0 +1,19 @@
+import type { Social } from "@/types";
+
+export const socialAccounts: Array<Social> = [
+  {
+    name: "GitHub",
+    url: "https://github.com/Paul2G",
+    icon: "ph-fill ph-github-logo",
+  },
+  {
+    name: "LinkedIn",
+    url: "https://www.linkedin.com/in/paul2g/",
+    icon: "ph-fill ph-linkedin-logo",
+  },
+  {
+    name: "Email",
+    url: "mailto:pggaleana@gmail.com",
+    icon: "ph-fill ph-envelope",
+  },
+];
---
+++ src/data/navigation.ts
@@ -0,0 +1,7 @@
+export const navigationItems = [
+  { label: "Home", to: "#" },
+  { label: "Experience", to: "#experience" },
+  { label: "Projects", to: "#projects" },
+  { label: "Education", to: "#education" },
+  { label: "About", to: "#about" },
+] as const;
