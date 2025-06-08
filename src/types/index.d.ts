d846d5a3bbf44bd65cdc1818204709b3cf718f16
Paul2G <pggaleana@gmail.com>
2025-06-08 01:56:09 -0700

    feat: create data types

---
+++ src/types/index.d.ts
@@ -0,0 +1,36 @@
+import { AstroComponentInstance } from "astro/runtime/server";
+
+export interface Company {
+  name: string;
+  website: string | URL;
+}
+
+export interface ExperienceEvent {
+  title: string;
+  description: string;
+  bullets: Array<string>;
+  company: Company;
+  startDate: Date;
+  endDate: Date;
+}
+
+export interface Technology {
+  name: string;
+  icon: AstroComponentInstance;
+}
+
+export interface Project {
+  title: string;
+  image?: string;
+  description: string;
+  technologies: Array<Technology>;
+  repository?: string;
+  liveDemo?: string;
+}
+
+export interface Social {
+  name: string;
+  url: string | URL;
+  // Phosphor icon xd
+  icon: string;
+}
