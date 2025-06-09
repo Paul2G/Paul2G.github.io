05804efd43f2956d6f799986bc54a45bd786aeb3
Paul2G <pggaleana@gmail.com>
2025-06-08 23:22:13 -0700

    style: run prettier format

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,25 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-  darkMode: 'class',
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: "class",
   theme: {
     extend: {
       fontFamily: {
-        mono: ['"Space Mono"', 'monospace'],
-        sans: ['"Space Grotesk Variable"', 'sans-serif'],
+        mono: ['"Space Mono"', "monospace"],
+        sans: ['"Space Grotesk Variable"', "sans-serif"],
       },
       colors: {
-        primary: '#0066FF',
-        secondary: '#121212',
-        tertiary: '#F5F5F5',
+        primary: "#0066FF",
+        secondary: "#121212",
+        tertiary: "#F5F5F5",
       },
       animation: {
-        'cursor-blink': 'cursor-blink 1s step-end infinite',
+        "cursor-blink": "cursor-blink 1s step-end infinite",
       },
       keyframes: {
-        'cursor-blink': {
-          '0%, 100%': { opacity: 1 },
-          '50%': { opacity: 0 },
+        "cursor-blink": {
+          "0%, 100%": { opacity: 1 },
+          "50%": { opacity: 0 },
         },
       },
     },
